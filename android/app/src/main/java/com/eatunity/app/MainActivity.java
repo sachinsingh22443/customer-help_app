43470b2b6d7941f85423c9e51553bd06a2322a8f
sachin singh <sachinsingh22443@gmail.com>
2026-06-08 12:23:35 +0530

    a

--- android/app/src/main/java/com/eatunity/app/MainActivity.java
+++ android/app/src/main/java/com/eatunity/app/MainActivity.java
@@ -8,8 +8,7 @@ public class MainActivity extends BridgeActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
         registerPlugin(Checkout.class);
+        super.onCreate(savedInstanceState);
     }
 }
